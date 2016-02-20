b8461ae296b52d1e12bf43d87fabe6e27b77ed05
陈英鹏 <chenyingpeng@chenyingpengdeMac-2.local>
2016-02-20 09:54:00 +0800

    Initial Commit

---
+++ 登录text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  登录text
+//
+//  Created by 陈英鹏 on 16/2/20.
+//  Copyright (c) 2016年 LYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 登录text/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  登录text
+//
+//  Created by 陈英鹏 on 16/2/20.
+//  Copyright (c) 2016年 LYY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
